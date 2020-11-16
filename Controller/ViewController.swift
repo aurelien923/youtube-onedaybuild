f43d57f86eca8b278a8b74faa4324db6ce3a85fa
Aurélien Alcuvilla <aurelienalcuvilla@gmail.com>
2020-11-16 22:55:18 +0100

    initial xcode project creation

---
+++ Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Aurélien Alcuvilla on 16/11/2020.
+//  Copyright © 2020 Aurelien Alcuvilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
